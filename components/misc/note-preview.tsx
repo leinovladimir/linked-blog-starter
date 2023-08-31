9af4c41146fa4c608ca2a5baf56b2e352a1e4904
Leino <leinovladimir@gmail.com>
2023-09-01 00:36:06 +0400

    modifi backlinks

--- components/misc/note-preview.tsx
+++ components/misc/note-preview.tsx
@@ -3,13 +3,27 @@ type Props = {
   content: string
 }
 
+function TruncateText({ content, maxLength }) {
+  const truncatedContent =
+    content.length > maxLength
+      ? content.substr(0, maxLength) + '...'
+      : content;
+
+  return <div>{truncatedContent}</div>;
+}
+
 const NotePreview = ({ title, content }: Props) => {
   return (
-    <span className="note-preview block col-span-2 max-w-[400px] rounded shadow-sm p-5 bg-white cursor-pointer text-lg hover:bg-gray-200 hover:border-transparent">
-      <span className="block font-bold leading-snug tracking-tight truncate mb-1">{title}</span>
-      <span className="block font-normal text-gray-600 whitespace-pre-line max-h-[150px] truncate">{content}</span>
+    <span className="note-preview block col-span-2 max-w-[400px] rounded shadow-sm p-5 bg-white cursor-pointer text-lg hover:bg-slate-50">
+      <span className="block font-bold leading-snug tracking-tight mb-1">
+        <TruncateText content={title} maxLength={100} />
+      </span>
+      <span className="block font-normal text-gray-600 whitespace-pre-line">
+        <TruncateText content={content} maxLength={160} />
+      </span>
     </span>
-  )
+  );
 }
 
 export default NotePreview
+
